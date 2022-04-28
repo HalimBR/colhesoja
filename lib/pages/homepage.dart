8f0aaacc03d5698cbc1e70083beb7d96b1f11cd1
Unknown <lagohalim1@gmail.com>
2022-04-28 11:19:33 -0300

    28-04

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:colhesoja/controllers/drawer.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({ Key? key }) : super(key: key);
@@ -27,71 +28,202 @@ class _HomePageState extends State<HomePage> {
           )
         ),
       child: Scaffold(
-        drawer: MenuDrawer(),
+        drawer: const MenuDrawer(),
         backgroundColor: Colors.transparent,
         appBar: AppBar(
           elevation: 0,
-          shadowColor: Colors.transparent,
-          backgroundColor: Colors.transparent,
-          title: const Center(child: Text("ColheSoja"))
+          backgroundColor: const Color.fromRGBO(81, 34, 13, 1),
+          title:  Text("ColheSoja", style: GoogleFonts.iceberg())
         ),
         body: Container(
           color: Colors.transparent,
           child: Column(
             children: [
-              Expanded(
-                flex: 1,
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.05,
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Card(
-                      elevation: 8,
+                    Expanded(
                       child: Container(
-                        child: Text("Cotação Soja"),
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        child: Center(child: Text("SOJA", style: GoogleFonts.iceberg(fontSize: 20),)),
+                        decoration: BoxDecoration(
+                          color: const Color.fromRGBO(187, 135, 75, 1),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2
+                          )
+
+                        ),
                       ),
                     ),
-                    Card(
-                      elevation: 8,
+                    Expanded(
                       child: Container(
-                        child: Text("Cotação Dolar"),
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        child: Center(child: Text("TEMPO", style: GoogleFonts.iceberg(fontSize: 20),)),
+                        decoration: const BoxDecoration(
+
+                          color: Color.fromRGBO(187, 135, 75, 1),
+                          border:  Border(
+                            top:  BorderSide(
+                              color: Colors.black,
+                              width: 2
+                            ),
+                            bottom: BorderSide(
+                              color: Colors.black,
+                              width: 2
+                            ),
+                          )
+
+                        ),
                       ),
-                    )
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: MediaQuery.of(context).size.height * 0.05,
+                        child: Center(child: Text("MOEDA", style: GoogleFonts.iceberg(fontSize: 20),)),
+                        decoration: BoxDecoration(
+                          color: const Color.fromRGBO(187, 135, 75, 1),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 2
+                          )
+
+                        ),
+                      ),
+                    ),
                   ],
-                )
+                ),
               ),
               Expanded(
-                flex: 5,
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    Card(
-                      child: Container(
-                        child: Text("Capacidade Operacional"),
-                      ),
-                    ),
-                     Card(
-                      child: Container(
-                        child: Text("Custo Horario"),
-                      ),
-                    ),
-                     Card(
-                      child: Container(
-                        child: Text("Estimativa Produtividade"),
-                      ),
-                    ),
-                     Card(
-                      child: Container(
-                        child: Text("Perdas"),
-                      ),
-                    )
-                  ],
-                )
-              ),
-              Expanded(
-                flex: 3,
                 child: Container(
-                )
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      tileMode: TileMode.clamp,
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromRGBO(187, 135, 75, 1),
+                        
+                        Colors.white
+                      ]
+                    ),
+                    image: DecorationImage(
+                      alignment: Alignment.bottomCenter,
+                      image: AssetImage("assets/fundo.png")
+                    )
+                  ),
+                  
+                ),
               ),
+              Expanded(
+                child: Container(
+                  width: MediaQuery.of(context).size.height,
+                  color: const Color.fromRGBO(242, 226, 171, 1),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Text("FUNCIONALIDADES", style: GoogleFonts.inter(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 15, right: 15),
+                        child: Text("Selecione um dos recursos abaixo para obter maiores informações sobre a colheita mecanizada da soja", style: GoogleFonts.inter(
+                          color: Colors.black,
+                          fontSize: 10,
+                        ),),
+                      ),
+
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.only(
+                            bottom: 25,
+                            left: 15,
+                            right: 15,
+                            top: 15
+                          ),
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+
+                               Card(
+                                color: const Color.fromRGBO(115, 97, 47, 1),
+                                elevation: 5,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10) ),
+                                child: SizedBox(
+                                  width: MediaQuery.of(context).size.width * 0.3,
+                                  child: Column(
+                                    children: [
+                                      Expanded(
+                                        child: SizedBox(),
+                                      ),
+                                      Image.asset("assets/perdas.png", 
+                                        
+                                        
+                                        
+                                        )
+                                    ],
+                                  ),
+                                  
+                                  
+                                ),
+                              ),
+                              Card(
+                                color: const Color.fromRGBO(115, 97, 47, 1),
+                                elevation: 5,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10) ),
+                                child: SizedBox(
+                                  width: MediaQuery.of(context).size.width * 0.3,
+                                  child: Column(
+                                    children: [
+                                      Expanded(
+                                        child: SizedBox(),
+                                      ),
+                                      Image.asset("assets/estimativas.png", 
+                                        cacheWidth: 400,
+                                        
+                                        )
+                                    ],
+                                  ),
+                                  
+                                  
+                                ),
+                              ),
+                              Card(
+                                color: const Color.fromRGBO(115, 97, 47, 1),
+                                elevation: 5,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10) ),
+                                child: SizedBox(
+                                  width: MediaQuery.of(context).size.width * 0.3,
+                                  
+                                  
+                                ),
+                              ),
+                              Card(
+                                color: const Color.fromRGBO(115, 97, 47, 1),
+                                elevation: 5,
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10) ),
+                                child: SizedBox(
+                                  width: MediaQuery.of(context).size.width * 0.3,
+                                  
+                                  
+                                ),
+                              )
+                              
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         )
